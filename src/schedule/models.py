29e347a4cac05e668475fc6a2f94393e49f5ceb4
Jean-François SUBRINI <jean-francoissubrini@MacBook-Pro-de-Jean-Francois.local>
2021-11-24 22:53:42 +0100

    models for the stocks and schedule apps

--- src/schedule/models.py
+++ src/schedule/models.py
@@ -1,3 +1,70 @@
+"""All the models for the schedule app of the pos_vaiatea project.
+
+    Models: Guest and Trip.
+    """
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+GENDER = (
+    ("Male", "Male"),
+    ("Female", "Female"),
+)
+DIVING_LEVEL = (
+    ("Open Water", "Open Water"),
+    ("Advanced OW", "Advanced OW"),
+    ("Rescue Diver", "Rescue Diver"),
+    ("Divemaster", "Divemaster"),
+    ("Instructor", "Instructor"),
+    ("Master Scuba Diver", "Master Scuba Diver"),
+)
+
+
+class Guest(models.Model):
+    """To create the Guest table."""
+
+    first_name = models.CharField("Prénom", max_length=40)
+    last_name = models.CharField("Nom", max_length=40)
+    gender = models.CharField(
+        "Genre", max_length=10, choices=GENDER)
+    age = models.PositiveSmallIntegerField(
+        "Age")
+    nationality = models.CharField(
+        "Nationalité", max_length=30)
+    passport_number = models.CharField(
+        "Numéro de passeport", max_length=15, blank=True, null=True)
+    diving_level = models.CharField(
+        "Niveau de plongée", max_length=20, choices=DIVING_LEVEL)
+    dives_number = models.PositiveSmallIntegerField(
+        "Nombre de plongées", blank=True, null=True)
+    email = models.CharField(
+        "Email", max_length=100)
+
+    class Meta:
+        verbose_name = "Invité(e)"
+
+    def __str__(self):
+        return f"Nom de l'invité(e) {self.first_name} {self.last_name}"
+
+
+class Trip(models.Model):
+    """To create the Trip table."""
+
+    itinerary = models.CharField(
+        "Itinéraire du liveaboard", max_length=40)
+    duration_days = models.PositiveSmallIntegerField(
+        "Durée en jours")
+    starting_date = models.DateField("Date de départ du liveaboard")
+    ending_date = models.DateField("Date d'arrivée du liveaboard")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="trips")
+
+    guests = models.ManyToManyField(Guest)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Voyage"
+
+    def __str__(self):
+        return f"Voyage {self.itinerary}"
--- src/stocks/models.py
+++ src/stocks/models.py
@@ -1,3 +1,124 @@
-from django.db import models
+"""All the models for the stocks app of the pos_vaiatea project.
 
-# Create your models here.
+    Models: Item, Drink, Goodies, Food and Stock.
+    """
+from django.db import models
+from schedule.models import Trip
+
+
+CATEGORY = (
+    ("Tee-Shirt", "Tee-Shirt"),
+    ("Sweat-Shirt", "Sweat-Shirt"),
+    ("Cap", "Cap"),
+)
+SIZE = (
+    ("XS", "XS"),
+    ("S", "S"),
+    ("M", "M"),
+    ("L", "L"),
+    ("XL", "XL"),
+    ("XXL", "XXL"),
+)
+GENDER = (
+    ("Male", "Male"),
+    ("Female", "Female"),
+    ("Unisex", "Unisex"),
+)
+FOOD_CATEGORY = (
+    ("Viande", "Viande"),
+    ("Poisson", "Poisson"),
+    ("Légumes", "Légumes"),
+    ("Blabla", "Blabla"),
+)
+TYPE_OF_ITEM = (
+    ("Bootle & Can", "Bootle & Can"),
+    ("Food", "Food"),
+    ("Goodies", "Goodies"),
+)
+
+
+class Drink(models.Model):
+    """To create the Drink table."""
+
+    price_unit_dollar = models.DecimalField(
+        "Prix unitaire en dollar", max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Boisson"
+
+    def __str__(self):
+        return f"Boisson au prix unitaire en dollar de {self.price_unit_dollar}"
+
+
+class Goodies(models.Model):
+    """To create the Goodies table."""
+
+    category = models.CharField(
+        "Catégorie", max_length=20, choices=CATEGORY)
+    size = models.CharField(
+        "Taille", max_length=5, choices=SIZE)
+    color = models.CharField(
+        "Couleur", max_length=30)
+    gender = models.CharField(
+        "Genre", max_length=10, choices=GENDER)
+    price_unit_dollar = models.DecimalField(
+        "Prix unitaire en dollar", max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Goodies"
+
+    def __str__(self):
+        return f"Goodies de catégorie {self.category}"
+
+
+class Food(models.Model):
+    """To create the Food table."""
+
+    food_category = models.CharField(
+        "Catégorie de nourriture", max_length=20, choices=FOOD_CATEGORY)
+
+    class Meta:
+        verbose_name = "Nourriture"
+
+    def __str__(self):
+        return f"Nourriture de catégorie {self.food_category}"
+
+
+class Item(models.Model):
+    """To create the Item table."""
+
+    name = models.CharField("Nom de l'article", max_length=30)
+    drink = models.OneToOneField(
+        Drink, on_delete=models.CASCADE, verbose_name="Boisson")
+    goodies = models.OneToOneField(
+        Goodies, on_delete=models.CASCADE, verbose_name="Goodies")
+    food = models.OneToOneField(
+        Food, on_delete=models.CASCADE, verbose_name="Nourriture")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Article"
+
+    def __str__(self):
+        return f"Article nommé {self.name}"
+
+
+class Stock(models.Model):
+    """To create the Stock table."""
+
+    type_of_item = models.CharField(
+        "Type d'article", max_length=20, choices=TYPE_OF_ITEM)
+    quantity = models.PositiveSmallIntegerField(
+        "Quantité", blank=True, null=True)
+    item = models.ForeignKey(
+        Item, on_delete=models.CASCADE, related_name="stocks", verbose_name="Article")
+    trips = models.ManyToManyField(Trip)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Stock"
+
+    def __str__(self):
+        return f"Stock de catégorie {self.type_of_item}"
