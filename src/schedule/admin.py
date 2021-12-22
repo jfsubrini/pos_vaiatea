32aacbe47d82a75f210fbaac736cb713239c5246
Jean-François SUBRINI <jean-francoissubrini@MacBook-Pro-de-Jean-Francois.local>
2021-12-22 22:32:40 +0100

    Auto calculation of the ending date in trip creation.

--- src/schedule/admin.py
+++ src/schedule/admin.py
@@ -2,6 +2,7 @@
 # pylint: disable=missing-class-docstring, no-member
 """All the admin pages to create, update, delete and read the trips and guests.
     """
+from datetime import timedelta
 from django.contrib import admin
 from .models import Guest, Trip
 
@@ -29,7 +30,7 @@ class GuestAdmin(admin.ModelAdmin):
 
 @ admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
-    exclude = ("user_id",)
+    exclude = ("user_id", "ending_date")
     list_display = ("itinerary", "duration_days",
                     "starting_date", "ending_date")
     list_filter = ("starting_date",)
@@ -38,4 +39,6 @@ class TripAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         obj.user_id = request.user
+        obj.ending_date = form.cleaned_data.get('starting_date') + \
+            timedelta(days=form.cleaned_data.get('duration_days') - 1)
         super().save_model(request, obj, form, change)
